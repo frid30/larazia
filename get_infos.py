690fb0c8f066f693d914bfe893c8ccfe3003a02a
frid30 <you@example.com>
2023-02-01 12:16:11 +0100

    azza

--- get_infos.py
+++ get_infos.py
@@ -2,10 +2,14 @@ from faker import Faker
 
 fake = Faker()
 
-class Name:
+class Feed:
     def __init__(self) -> None:
         pass
     def name(self):
+        return fake.first_name()
+    def password(self):
+        return fake.password()
+    def firstname(self):
         return fake.first_name()
 class Email:
     def __init__(self) -> None:
