0822768d37f8627f3ca81eb7870924a276b21d45
Naïm Bouferrache <bouferrachenaim@gmail.com>
2023-02-02 14:10:10 +0100

    ggggggggggg

--- sign_up/yahoo.py
+++ sign_up/yahoo.py
@@ -19,14 +19,14 @@ options.add_argument("--start-maximized")
 options.add_argument("--no-sandbox")
 
 
-def click_on(self, xpath):
+def click_on(xpath):
     try:
         driver.implicitly_wait(6)
         driver.find_element(By.XPATH, xpath).click()
         time.sleep(1)
     except:
         pass
-def fill(self, xpath, value):
+def fill(xpath, value):
     try:
         driver.implicitly_wait(6)
         searchButton = driver.find_element(By.XPATH, xpath)
